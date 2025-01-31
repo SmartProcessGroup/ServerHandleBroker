74d97daf81a9a5bee248fa770cd7bcbcc7b24164
Jesus M <jmontero@smartprocessgroup.com>
2025-01-31 11:45:30 -0500

    Se cambia la estructura para enviar a la BD

--- AWS/DynamoDb/AddItems.js
+++ AWS/DynamoDb/AddItems.js
@@ -1,30 +1,52 @@
 const { DynamoDBDocumentClient, UpdateCommand } = require("@aws-sdk/lib-dynamodb");
+const { PutCommand } = require("@aws-sdk/lib-dynamodb");
 const dynamoDBClient  = require("./ClientDynamoDB.js");
 const commands = require("../../Utils/Commands.js");
 
-async function addItem(NameTable, ID, message) {
+function setParams(NameTable,ID,Fecha,Type,Data){
+	return {
+		TableName: NameTable,
+		Item:{
+			ID: ID,
+			Fecha: Fecha,
+			type: Type,
+			Data: Data
+		}
+	}
 
+}
+
+async function addItem(NameTable, ID, message) {
     const docClient = DynamoDBDocumentClient.from(dynamoDBClient);
     const data = splitData(message);    //Data a Guardar
-    const date = Math.floor(Date.now() / 1000).toString(); //Fecha de la data
-    const params = {
-        TableName: NameTable,
-        Key: { ID },
-        UpdateExpression: "SET #values.#date = :data, pos = :location",
-        ExpressionAttributeNames: { "#values": "values", "#date": date},
-        ExpressionAttributeValues: { ":data": data, ":location": message.pos}, 
-        ReturnValues: "ALL_NEW", 
-    };
-    
-    try {
-        const response = await docClient.send(new UpdateCommand(params));
-        console.log("Elemento actualizado");
-        return response.Attributes;
-        
-    } catch (error) {
-        console.error("Error al añadir el elemento:", error.message);
+    const date = Math.floor(Date.now() / 1000)//.toString(); //Fecha de la data
+
+		try {
+			await docClient.send(new PutCommand(setParams(NameTable,ID,date,"data",data)))
+			// await docClient.send(new PutCommand(setParams(NameTable,ID,date,"location",message.pos)))
+			console.log("Valores Actualizados");
+			
+			
+		} catch (error) {
+			console.error("Error al añadir el elemento:", error.message);
         throw error;
-    }
+		}
+		
+    // try {
+		// 		//Send Data
+    //     var response = await docClient.send(new UpdateCommand(params));
+		// 		//Send Location
+		// 		type = "location"
+		// 		data = message.pos
+		// 		params.UpdateExpression = "SET #date = :date, #type = :type, #values = :data";
+    //     response = await docClient.send(new UpdateCommand(params));
+    //     console.log("Elemento actualizado");
+    //     return response.Attributes;
+        
+    // } catch (error) {
+    //     console.error("Error al añadir el elemento:", error.message);
+    //     throw error;
+    // }
 }
 
 function splitData(data) {
